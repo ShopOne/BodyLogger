d517b240d6896b2eec8fd0e4213cd7de2068a1b0
ShopOne <oneky8080@gmail.com>
2020-08-04 23:26:50 +0900

    create card layout

--- app/src/main/java/com/support/bodylogger/MainActivity.kt
+++ app/src/main/java/com/support/bodylogger/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.support.bodylogger
 
+import BodyInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dummy = List<BodyInfo>(5){i -> BodyInfo(i,i,Date(),List(2){j->"$j $i"})}
+        my_recyclerview.layoutManager = LinearLayoutManager(this)
+
+
     }
 }
